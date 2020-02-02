86e48e2fc4fd1bf49a21a2bef1e71ae3e77ec79d
Albert <albertfarhat@gmail.com>
2020-02-02 17:12:34 +0200

    Reordered files

---
+++ ApiKeyAuth/Controllers/Auth.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPISecurity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult NotAuthorized()
+        {
+            return NotFound();
+        }
+}
+}
