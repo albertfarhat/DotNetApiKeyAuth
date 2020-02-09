1e1e7192b823d1ab087c8977221f45e22281924c
Albert <albertfarhat@gmail.com>
2020-02-09 07:33:30 +0200

    Added GlobalSettings to allow changes to static values

---
+++ src/ApiKeyAuth/GlobalSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiKeyAuth
+{
+    internal static class GlobalSettings
+    {
+        public static string HeaderKey { get; set; }
+        public static string AuthController { get; set; } = "Auth";
+        public static string NotAuthorizedAction { get; set; } = "NotAuthorized";
+        public static string NoApiKeyAction { get; set; } = "Forbidden";
+        public static string NoApiKeyReasonPhrase { get; set; } = "NoApiKey";
+        public static string NotAuthorizedReasonPhrase = "NotAuthorized";
+    }
+}
